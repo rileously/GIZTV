5871ea8e5982cbadf3daa426a7360212c354c8d3
rileously <eddamrilwan@gmail.com>
2026-08-05 00:44:32 +0500

    Release 1.12.3

--- AuroraTV/app/build.gradle.kts
+++ AuroraTV/app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.example.auroratv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 54
-        versionName = "1.12.2"
+        versionCode = 55
+        versionName = "1.12.3"
         buildConfigField("String", "TMDB_API_KEY", "\"$escapedTmdbApiKey\"")
         buildConfigField("String", "UPDATE_MANIFEST_URL", "\"$escapedUpdateManifestUrl\"")
     }
